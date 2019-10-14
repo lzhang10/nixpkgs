0af502adbd8546906436724bbaa8f420a122d72a
Le Zhang <you@example.com>
2019-10-14 15:03:33 -0400

    nixos-19.09: patch x2goclient to resume after suspending

--- pkgs/applications/networking/remote/x2goclient/default.nix
+++ pkgs/applications/networking/remote/x2goclient/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchgit, cups, libssh, libXpm, nx-libs, openldap, openssh
+{ stdenv, fetchgit, fetchpatch, cups, libssh, libXpm, nx-libs, openldap, openssh
 , mkDerivation, qtbase, qtsvg, qtx11extras, qttools, phonon, pkgconfig }:
 
 mkDerivation {
@@ -10,6 +10,14 @@ mkDerivation {
    rev = "704c4ab92d20070dd160824c9b66a6d1c56dcc49";
    sha256 = "1pndp3lfzwifyxqq0gps3p1bwakw06clbk6n8viv020l4bsfmq5f";
   };
+
+  patches = [
+    (fetchpatch {
+      sha256 = "1ljsryswqvd27fvhwyg3sw3qm2xi0nhz6lrp3q7sb3imi9rjwh2m";
+      name = "x2goclient-resume-after-suspending.patch";
+      url = "https://gist.githubusercontent.com/lzhang10/59f12e5df1ae78e4d3a6ddda75f5ab32/raw/66d9fcc60327bae5d7fd6e9b62e2a0a2f9df8588/x2goclient-resume-after-suspending.patch";
+    })
+  ];
 
   buildInputs = [ cups libssh libXpm nx-libs openldap openssh
                   qtbase qtsvg qtx11extras qttools phonon pkgconfig ];
